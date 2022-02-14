b501bb58c2c4108329f9fda4645583f829f3e53d
Kiara Kaiser <kiarakaiser1@gmail.com>
2022-02-14 20:06:15 +0100

    Initial commit

---
+++ test.php
@@ -0,0 +1,16 @@
+<?php
+
+
+
+?>
+
+<form action="/addUser.php" method="post">
+    <label for="fname">First name:</label><br>
+    <input type="text" id="fname" name="fname" value="John"><br>
+    <label for="lname">Last name:</label><br>
+    <input type="text" id="lname" name="lname" value="Doe"><br><br>
+    <label for="age">Last name:</label><br>
+    <input type="number" id="age" name="age" value="Doe"><br><br>
+
+    <input type="submit" value="Submit">
+</form>
